573b845a3d2198c65632a508f18cdde0d79723e8
Jon-Mailes Graeffe <mail@jonni.it>
2024-11-03 18:57:33 +0100

    initial commit

---
+++ MaxTeamPlayersOverridePlugin.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json.Serialization;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+
+namespace MaxTeamPlayersOverridePlugin;
+
+public class PluginConfig : BasePluginConfig
+{
+    [JsonPropertyName("max_ts")]
+    public int MaxTs { get; set; } = -1;
+    
+    [JsonPropertyName("max_cts")]
+    public int MaxCTs { get; set; } = -1;
+}
+
+public class MaxTeamPlayersOverridePlugin : BasePlugin, IPluginConfig<PluginConfig>
+{
+    public override string ModuleName => "Max Team Players Override Plugin";
+    public override string ModuleVersion => "1.0.0";
+
+    public PluginConfig Config { get; set; }
+    
+    public void OnConfigParsed(PluginConfig? config)
+    {
+        if (config == null) return;
+        Config = config;
+    }
+    
+    public override void Load(bool hotReload)
+    {
+        RegisterEventHandler<EventRoundStart>((_, _) =>
+        {
+            var maxTs = Config.MaxTs;
+            var maxCTs = Config.MaxCTs;
+
+            if (maxTs < 0)
+            {
+                maxTs = Server.MaxPlayers / 2;
+            }
+            if (maxCTs < 0)
+            {
+                maxCTs = Server.MaxPlayers / 2;
+            }
+            
+            SetMaxTs(maxTs);
+            SetMaxCTs(maxCTs);
+            
+            return HookResult.Continue;
+        });
+    }
+    
+    private void SetMaxTs(int num)
+    {
+        var ents = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules");
+        foreach (var ent in ents)
+        {
+            ent.GameRules!.NumSpawnableTerrorist = num;
+            ent.GameRules.MaxNumTerrorists = num;
+        }
+    }
+
+    private void SetMaxCTs(int num)
+    {
+        var ents = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules");
+        foreach (var ent in ents)
+        {
+            ent.GameRules!.NumSpawnableCT = num;
+            ent.GameRules.MaxNumCTs = num;
+        }
+    }
+}
