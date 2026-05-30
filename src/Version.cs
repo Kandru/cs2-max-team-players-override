0662680f1057a0bac8420fc8c75483dd1d1600fc
derkalle4 <kalle@kandru.de>
2026-05-31 01:39:55 +0200

    bump version to 26.05.1

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace MaxTeamPlayersOverride
 {
     public partial class MaxTeamPlayersOverride
     {
-        public override string ModuleVersion => "26.04.1";
+        public override string ModuleVersion => "26.05.1";
     }
 }
